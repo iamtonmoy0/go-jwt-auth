9b855ad4b36af43a5e41a892f9b4bc27f61f5c7a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-21 14:52:50 -0400

    data model struct added  and validation set

--- models/userModel.go
+++ models/userModel.go
@@ -7,16 +7,16 @@ import (
 )
 
 type User struct {
-	ID            primitive.ObjectID
-	First_name    *string
-	Last_name     *string
-	Password      *string
-	Email         *string
-	Phone         *string
-	Token         *string
-	User_type     *string
-	Refresh_token *string
-	Created_at    time.Time
-	Updated_at    time.Time
-	User_id       string
+	ID            primitive.ObjectID `bson:"_id"`
+	First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Password      *string            `json:"password" validate:"required,min=6"`
+	Email         *string            `json:"email" validate:"required,email"`
+	Phone         *string            `json:"phone" validate:"required"`
+	Token         *string            `json:"token" `
+	User_type     *string            `json:"user_type" validate:"required,eq=Admin|eq=USER"`
+	Refresh_token *string            `json:"refresh_token" "`
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"updated_at"`
+	User_id       string             `json:"user_id"`
 }
