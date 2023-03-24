70c06414e14285b0049473be735fb8a02faf5dc4
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-24 17:12:16 -0400

    db connected

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,4 +25,20 @@ func DBinstance() *mongo.Client {
 		log.Fatal(err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
 }
