566ce1dc8bb643e36eededd865cbd6cd16c7b8d1
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-26 22:08:51 -0400

    email and phone validator added

--- controllers/userControllers.go
+++ controllers/userControllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
 
@@ -21,7 +22,37 @@ func HashPass()
 
 func VerifyPass()
 
-func Signup()
+func Signup() {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		validationErr = validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "error occured while checking for the email"})
+		}
+		count, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "error occured while checking for the phone number"})
+		}
+		//if the email and phone is already exist on the db
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email and phone number already exist"})
+		}
+
+	}
+}
 
 func Login()
 
@@ -36,7 +67,7 @@ func GetUser() gin.HandlerFunc {
 		}
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		//user struct from models
-		var user = models.User
+		var user models.User
 		err = userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
 		defer cancel()
 		if err != nil {
